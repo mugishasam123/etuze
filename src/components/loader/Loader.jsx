76022177713cdc6a5ff7c8d2d65d4162aa562965
JDMUHIRWA <j.hareriman@alustudent.com>
2024-11-12 14:50:42 +0200

    added Loader component

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <>
+      <div className="content flex justify-center">
+        <div className="loading bg-2"></div>
+      </div>
+    </>
+  )
+}
+
+export default Loader
