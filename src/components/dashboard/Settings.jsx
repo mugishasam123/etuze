994137fd6d2950a70c43890f8a2e5133fb78c94b
belladev250 <lissadev17@gmail.com>
2024-11-29 10:06:47 +0200

    resolved some of my conflicts

--- src/components/dashboard/Settings.jsx
+++ src/components/dashboard/Settings.jsx
@@ -4,7 +4,7 @@ const Settings = () => {
   return (
     <div className='flex justify-center items-center text-[gray] flex-col'>
       <h1 className='text-[40px] text-[black]'>Settings</h1>
-      <p >in Progress....</p>
+   <p>settings are being developed</p>
     </div>
   )
 }
